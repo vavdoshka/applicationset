84074148790b49f6b56927e0ef27378dd44baf65
Jonathan West <jonwest@redhat.com>
2022-02-07 11:13:55 -0500

    chore: Update ApplicationSet to latest Argo CD v2.3 dependencies
    Signed-off-by: Jonathan West <jonwest@redhat.com>

--- hack/generate-manifests.sh
+++ hack/generate-manifests.sh
@@ -46,7 +46,9 @@ cd ${SRCROOT} && chmod 644 manifests/install.yaml
 "${SRCROOT}/hack/verify-argo-cd-versions.sh"
 
 # Use kustomize to render 'manifests/install-with-argo-cd.yaml
-echo "${AUTOGENMSG}" > ${TEMPFILE}
-cd ${SRCROOT}/manifests/namespace-install-with-argo-cd && ${KUSTOMIZE} build . >> ${TEMPFILE}
-mv ${TEMPFILE} ${SRCROOT}/manifests/install-with-argo-cd.yaml
-cd ${SRCROOT} && chmod 644 manifests/install-with-argo-cd.yaml
+#echo "${AUTOGENMSG}" > ${TEMPFILE}
+#cd ${SRCROOT}/manifests/namespace-install-with-argo-cd && ${KUSTOMIZE} build . >> ${TEMPFILE}
+#mv ${TEMPFILE} ${SRCROOT}/manifests/install-with-argo-cd.yaml
+#cd ${SRCROOT} && chmod 644 manifests/install-with-argo-cd.yaml
+
+
